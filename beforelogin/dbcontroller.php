dc24d35fec4fade444c21eb843047fa51d8b196d
wishcream <wishcream6@gmail.com>
2023-05-27 01:30:19 +0700

    Add result when fail to connect database

--- beforelogin/dbcontroller.php
+++ beforelogin/dbcontroller.php
@@ -12,7 +12,7 @@ class DBController {
     }
 
     function connectDB() {
-        $conn = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+        $conn = mysqli_connect($this->host, $this->user, $this->password, $this->database) or die("Fail to connect");
         return $conn;
     }
 
