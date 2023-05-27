e89d6e05db97b0699ce8dd781f71e586646728f1
Mannoppakorn <man.noppakorn@gmail.com>
2023-05-28 03:10:36 +0700

    change server name

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=foodleehereproject", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=arherelee", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully";
