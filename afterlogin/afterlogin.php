38eb28c3236cd6042bc550a1993c073e57704547
Mannoppakorn <man.noppakorn@gmail.com>
2023-05-29 07:24:21 +0700

    user account setting

--- afterlogin/afterlogin.php
+++ afterlogin/afterlogin.php
@@ -51,7 +51,7 @@
                     <!-- <li><a href="blog.php">BLOG</a></li> -->
                     <li><a href="table.php">TABLE</a></li>
                     <li><a href="logout.php">LOGOUT</a></li>
-                    <li><a href="accountsetting.php">ACCOUNT SETTING</a></li>
+                    <li><a href="accountsetting2.php?edit=<?php echo $_SESSION['user_login']; ?>">ACCOUNT SETTING</a></li>
                     <li id="lg-bag"><a href="afcart.php"><i class='bx bx-shopping-bag'></i></a></li>
                     <a href="#" id="close"><i class="fa-solid fa-xmark"></i></a>
                 </ul>
